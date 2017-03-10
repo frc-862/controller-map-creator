b1d501590156a0d47a0aecb5894e93584f56091b
Marcus Stojcevich <mstojcevich@gmail.com>
2017-03-10 17:47:10 -0500

    Web server for updating, to use as a github webhook

---
+++ updater-server.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import annotator
+import urllib.request
+
+CONFIG_URL = 'https://raw.githubusercontent.com/frc-862/valkyrie/master/valkyrie.yaml'
+
+app = Flask(__name__, static_folder='out')
+
+@app.route('/update-mapping')
+def update_mapping():
+    # Download the latest Valkyrie yaml
+    urllib.request.urlretrieve(CONFIG_URL, 'valkyrie.yaml')
+
+    # Create the annotated controller images
+    annotation = annotator.ControllerAnnotation('config.yaml')
+    annotation.create_mapping_files()
+
+    return 'Done'
+
+@app.route('/pdf')
+def serve_pdf():
+    return app.send_static_file('out.pdf')
+
+# Run the webapp if we're not a library
+if __name__ == "__main__":
+    app.run()
